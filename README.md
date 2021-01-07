3c2349b7020209ffc3c6a40f40cee3cda4ba6805
Ryan Conran <ryan.conran2@gmail.com>
2021-01-06 18:03:28 -0600

    Completed inquirer prompts and added additional connection querys

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # employee-tracker
+
+## Purpose:  This app was created to help input departments roles and employees into a exising mysql database using node and inquier to add the appropriate data.  This app can greatly reduce the
+time it takes to add personnel and company informtaiton to a database, rather than doing so manually with programs like excel.  
+
+## Installation instructions:  Users wishing to use this code must first install all of the appropriate modules using npm and then enter their password to connect the mysql database. Users with the code would then
+enter the node server.js in the terminal to begin the inquier prompts on their end.  Users will then work through the prompts using inputs and selecting from choices using the arrow keys.  Once the user has
+added a department, role or an employee, they will be directed to choose another prompt to repeat the process.  Once the information is entered, users will find the information now added to their mysql database.  
+
--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const mysql = require('mysql');
 
 let connection = null;
 let departments = [];
+let roles = [];
+let employees = [{fullName:"No Manager"}];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 function promptMenu() {
-    const mainMenu = [    
-        { 
+    const mainMenu = [
+        {
             type: 'list',
             message: 'What would you like to do?',
             name: 'menu',
-            choices: ["Add Departnment", "Add Role", "Add Employee"]
+            choices: ["Add Departnment", "Add Role", "Add Employee", "View Employees by Manager"]
         },
         {
             type: 'input',
@@ -54,69 +56,107 @@ function promptMenu() {
             when: (answers) => answers.menu === "Add Employee"
         },
         {
-            type: 'input',
-            message: 'Enter an employee Role',
-            name: 'roleId',
+            type: 'list',
+            message: 'Select role',
+            name: 'roleName',
+            choices: roles.map(d => d.title),
             when: (answers) => answers.menu === "Add Employee"
         },
         {
-            type: 'input',
-            message: 'Enter an employee Manager',
-            name: 'managerId',
+            type: 'list',
+            message: 'Select a manager',
+            name: 'managerName',
+            choices: employees.map(e => e.fullName),
             when: (answers) => answers.menu === "Add Employee"
         },
-    
-    ]; 
-    
+        {
+            type: 'list',
+            message: 'Select a manager',
+            name: 'managerName',
+            choices: employees.filter(e => e.manager != "No Manager").map(e => e.manager),
+            when: (answers) => answers.menu === "View Employees by Manager"
+        }
+
+    ];
+    // proimises after questions have been answered in terminal
     inquirer.prompt(mainMenu).then((answers) => {
-    
         console.log(answers);
 
         if (answers.menu === "Add Departnment") {
             const department = {
-                name: answers.name
+                name: answers.name,
             };
 
             const query = `INSERT INTO department(name) VALUES('${department.name}')`;
 
-            connection.query(query, function (err, result ) {
+            connection.query(query, function (err, result) {
                 if (err) throw err;
                 console.log("Department is inserted");
-            });            
+            });
         } else if (answers.menu === "Add Role") {
             const role = {
                 title: answers.title,
                 salary: answers.salary,
-                departmentId: departments.filter(d => d.name == answers.departmentName)[0].id
+                departmentId: departments.filter(
+                    (d) => d.name == answers.departmentName
+                )[0].id,
             };
 
             const query = `INSERT INTO role(title, salary, department_id) VALUES('${role.title}', ${role.salary}, ${role.departmentId} )`;
 
-            connection.query(query, function (err, result ) {
+            connection.query(query, function (err, result) {
                 if (err) throw err;
                 console.log("Role is inserted");
-            }); 
+            });
+        } else if (answers.menu == "Add Employee") {
+            let managerId = 'NULL';
+
+            if (answers.managerName != "No Manager") {
+                managerId = employees.filter(e => e.fullName == answers.managerName)[0].id;
+            }
+
+            const employee = {
+                firstName: answers.firstName,
+                lastName: answers.lastName,
+                roleId: roles.filter(
+                    (d) => d.title == answers.roleName
+                )[0].id,
+                managerId: managerId
+            };
+            const query = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('${employee.firstName}', '${employee.lastName}', ${employee.roleId}, ${managerId})`;
+
+            connection.query(query, function (err, result) {
+                if (err) throw err;
+                console.log("Employee Created");
+            });
+        } else {
+            console.log("Showing all employees");
+            for(let i=0; i< employees.length; i++)
+            {
+                if (employees[i].fullName == answers.managerName) {
+                    console.log(`Employee: ${employees[i].fullName}`);
+                }
+            }
         }
-    
+
         run();
     });
 }
 
-// Change to my information to access database 
 function createDBConnection() {
     connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : 'password',
-        database : 'employee_tracker'
-      });
-       
-      connection.connect();
+        host: 'localhost',
+        user: 'root',
+        password: 'Bridgehouse22!',
+        database: 'employee_tracker'
+    });
+
+    connection.connect();
 }
 
 function run() {
     departments = [];
-    connection.query("SELECT * FROM department", function(err, res) {
+    connection.query("SELECT * FROM department", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             const department = {
                 id: res[i].id,
@@ -124,8 +164,42 @@ function run() {
             }
             departments.push(department);
         }
-        promptMenu();
-      });
+        roles = [];
+        connection.query("SELECT * FROM role", function (err, res) {
+            for (var i = 0; i < res.length; i++) {
+                const role = {
+                    id: res[i].id,
+                    title: res[i].title,
+                }
+                roles.push(role);
+            }
+            employees = [{fullName:"No Manager"}];
+            connection.query("SELECT * FROM employee", function (err, res) {
+                for (var i = 0; i < res.length; i++) {
+                    const employee = {
+                        id: res[i].id,
+                        firstName: res[i].first_name,
+                        lastName: res[i].last_name,
+                        fullName: `${res[i].first_name} ${res[i].last_name}`,
+                        managerId: res[i].manager_id
+                    }
+                    employees.push(employee);
+                }
+
+                //fill managers object
+
+                for(let i=0; i< employees.length;i++) {
+                    const manager = employees.filter(e => e.id == employees[i].managerId);
+                    if (manager.length > 0) {
+                        employees[i].manager = manager[0].fullName;
+                    }
+                }
+
+                //console.log("employees",employees);
+                promptMenu();
+            });
+        });
+    });
 }
 
 createDBConnection();
